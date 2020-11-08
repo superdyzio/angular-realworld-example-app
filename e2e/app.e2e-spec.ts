26bd9b4949e00c5913af569a33f5677be6af8a02
superdyzio <perdekdawid@gmail.com>
2020-11-08 11:47:58 +0100

    added custom locator - navItemByText

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,7 +1,21 @@
 import { Ng2RealApp } from './app.po';
+import {by, element} from 'protractor';
 
 describe('ng-demo App', () => {
   let page: Ng2RealApp;
+
+  by.addLocator('navItemByText', (text, parentElement, rootSelector) => {
+    const parent = parentElement || document;
+    const navItems = parent.querySelectorAll('a.nav-link');
+    let result = null;
+    navItems.forEach(el => {
+      if (el.textContent.includes(text)) {
+        result = el;
+      }
+    });
+
+    return result;
+  });
 
   beforeEach(() => {
     page = new Ng2RealApp();
@@ -9,6 +23,11 @@ describe('ng-demo App', () => {
 
   it('should display message saying app works', async () => {
     await page.navigateTo();
+
+    const loginNavItem = await element(by.navItemByText('Sign in'));
+    console.log(await loginNavItem.getText());
+
+    expect(await loginNavItem.getAttribute('href')).toContain('/login');
     expect(await page.getParagraphText()).toContain('conduit');
   });
 });
