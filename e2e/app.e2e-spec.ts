8723b96ffa1f2be3bd0219bb955e7b2a150d2bbe
superdyzio <perdekdawid@gmail.com>
2020-11-08 15:07:23 +0100

    screenshots added

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,18 @@
 import { Ng2RealApp } from './app.po';
+import {browser, by, element} from 'protractor';
+import * as fs from 'fs';
+
+function saveScreenshot(data, filename: string) {
+  if (!fs.existsSync(`${__dirname}\\screenshots`)) {
+    fs.mkdirSync(`${__dirname}\\screenshots`);
+  }
+
+  fs.writeFileSync(
+    `${__dirname}\\screenshots\\${filename}.png`,
+    data,
+    {encoding: 'base64', flag: 'w'}
+  );
+}
 
 describe('ng-demo App', () => {
   let page: Ng2RealApp;
@@ -7,8 +21,10 @@ describe('ng-demo App', () => {
     page = new Ng2RealApp();
   });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toContain('conduit');
+  it('should display message saying app works', async () => {
+    await page.navigateTo();
+    saveScreenshot(await browser.takeScreenshot(), 'home-page');
+    saveScreenshot(await element(by.css('a.nav-link')).takeScreenshot(), 'home-nav-link');
+    expect(await page.getParagraphText()).toContain('conduit');
   });
 });
